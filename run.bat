02324210e4c55594545a1090b70c7f2d78ec149c
Javier <javier.godino@acqua.net.ar>
2019-10-31 16:37:34 -0300

    Update

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
-docker network rm microservice_atinanet
-xcopy /s tmp\*.* C:\tmp
-docker-compose up
+@echo off
+echo "Creating JDE Atina Server..."
+docker network rm jde_atinanet
+docker-compose up --no-start
+docker-compose start
+docker cp tmp/jde/config/JDV920 jdeatina-server:/tmp/config/JDV920
+docker cp tmp/jde/lib jdeatina-server:/tmp/jde
