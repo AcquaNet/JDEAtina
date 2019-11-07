dee9dc4b7c0b406bfb5c408391ef9494c2712909
Javier <javier.godino@acqua.net.ar>
2019-11-07 16:00:08 -0300

    Update

---
+++ tools/JDECreateToken.bat
@@ -0,0 +1,6 @@
+@echo off
+CALL java -jar JDECreateToken-1.0.0.jar
+ECHO ==========================================================
+ECHO Token has been copied to Clipboard
+ECHO ==========================================================
+PAUSE >nul
