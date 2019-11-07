242ebf62e12a8b1deffda078b6341499d760a60a
Javier <javier.godino@acqua.net.ar>
2019-11-07 10:26:29 -0300

    Update

---
+++ clean.bat
@@ -0,0 +1,15 @@
+@echo off
+echo --------------------------------------------
+echo Cleaning JDE Atina Server...
+echo --------------------------------------------
+echo   
+echo        Stopping containers...
+docker-compose stop
+echo        Removing containers...
+docker rm mule-server
+docker rm ngrok-server
+docker rm jdeatina-microserver
+echo        Removing images...
+docker rmi 92455890/ngrok-server:1.0.0
+docker rmi 92455890/mule-server:1.0.0
+docker rmi 92455890/jdeatina-microserver:1.0.0
