0dec95633a9b93b1b017c5505888ac13802a616f
Javier <javier.godino@acqua.net.ar>
2019-11-07 10:19:08 -0300

    Update

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 echo "Creating JDE Atina Server..."
 docker-compose up --no-start
-docker-compose start
 docker cp tmp/jde/config/JDV920 jdeatina-microserver:/tmp/jde/config
 docker cp tmp/jde/config/JDV920 jdeatina-microserver:/tmp/jde
 docker cp tmp/jde/lib jdeatina-microserver:/tmp/jde
+docker-compose start
