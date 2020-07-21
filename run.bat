10caa53ca3a30a1b02a24824d8aeab6a2d421e11
fjgodino <fjgodino@gmail.com>
2020-07-21 12:26:42 -0300

    New JDE Instance

--- run.bat
+++ run.bat
@@ -4,3 +4,9 @@ docker-compose up --no-start
 docker cp tmp/jde/config/JDV920 jdeatina-microserver:/tmp/jde/config
 docker cp tmp/jde/lib jdeatina-microserver:/tmp/jde/lib
 docker-compose start
+#
+# docker exec -it jdeatina-microserver
+#
+#docker exec -it jdeatina-microserver /bin/bash
+#echo "65.52.119.187 JDE-ALPHA-SQL" >> /etc/hosts
+#echo "138.91.73.161 JDE-ALPHA-ENT" >> /etc/hosts
